6ed94af358b30c728980e7f4aaa21e82aecf6240
wael <fouzri_wael@hotmail.fr>
2016-10-28 17:09:21 +0200

    implements function win_small_board

--- board.c
+++ board.c
@@ -13,13 +13,22 @@ void init_board(board boards[], int size){
 	}
 }
 
+void win_small_board(board boards[], int size){
+int i,cpt = 0;
+	for(i=0; i<BOARD_DIM; i++){
+		if ((boards[cpt].b[i][0] =="x" || boards[cpt].b[i][0]== "o") && (boards[cpt].b[i][0]==boards[cpt].b[i][1] && boards[cpt+1].b[i][0]==boards[cpt].b[i][2])){
+			printf("you win"); }
+
+}
+
+
 void display_board(board boards[], int size){
 	int i, cpt=0;
 	while(cpt<size){
 		for(i=0; i<BOARD_DIM; i++){
 			printf("%c%c%c %c%c%c %c%c%c \n", boards[cpt].b[i][0], boards[cpt].b[i][1], boards[cpt].b[i][2],
-			boards[cpt+1].b[i][0], boards[cpt+1].b[i][1], boards[cpt+1].b[i][2],
-			boards[cpt+2].b[i][0], boards[cpt+2].b[i][1], boards[cpt+2].b[i][2]);
+											boards[cpt+1].b[i][0], boards[cpt+1].b[i][1], boards[cpt+1].b[i][2],
+											boards[cpt+2].b[i][0], boards[cpt+2].b[i][1], boards[cpt+2].b[i][2]);
 		}
 		cpt+=BOARD_DIM;
 		printf("\n");
