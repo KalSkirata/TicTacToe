5c8ded86f89b0a725be93f124a48f0b93fb9e86d
Guillaume <guillaume.taylor@gmail.com>
2016-11-11 17:57:07 +0100

    add stack file

---
+++ stack.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include "stack.h"
+
+int push(stack **s, int x, int y){
+        stack *element = malloc(sizeof(s));
+        if(!element) return -1;     /* Si l'allocation a échouée. */
+        element->x = x;
+        element->y = y;
+        element->prec = *s;
+        *s = element;       /* Le pointeur pointe sur le dernier élément. */
+        return 0;
+}
+
+void clear(stack **s){
+        stack *tmp;
+        while(*s)
+          {
+             tmp = (*s)->prec;
+             free(*s);
+             *s = tmp;
+          }
+}
+
+void display(stack *s){
+	while(s){
+		printf("%d %d\n",s->x,s->y);
+		s = s->prec;
+	}
+}
---
+++ stack.h
@@ -0,0 +1,12 @@
+struct stack{
+	int x,y;
+	struct stack *prec;
+};
+
+typedef struct stack stack;
+
+int push(stack **s, int x, int y);
+
+void clear(stack **s);
+
+void display(stack *s);
