a5213fa33e13c6693085e37eae7426f367467ff6
Guillaume <guillaume.taylor@gmail.com>
2016-11-26 16:14:23 +0100

    play with board won

--- move.c
+++ move.c
@@ -5,13 +5,22 @@
 int id[3][3] = {{0,1,2},{3,4,5},{6,7,8}}; //id for each cell of board
 
 int move_available(move previous_move, board boards[], int size){
-	int board_id, x, y, cpt=0;
+	int board_id, x, y, cpt=0,i;
 	
 	available = NULL;
 
 	//define board to play
 	board_id = id[previous_move.x][previous_move.y];
 	printf("board_id=%d pm.x=%d pm.y=%d \n", board_id, previous_move.x, previous_move.y);
+	if(boards[board_id].winner=='x' || boards[board_id].winner=='o'){
+		for(i=0; i<9; i++){ //parcours tous les plateaux
+			if(boards[i].winner!='x' && boards[i].winner!='o'){
+				board_id=i;
+				printf("board_id changes to %d \n", i);
+				break;
+			}
+		}
+	}
 	//search the empty cells
 	for(x=0; x<BOARD_DIM; x++){
 		for(y=0; y<BOARD_DIM; y++){
